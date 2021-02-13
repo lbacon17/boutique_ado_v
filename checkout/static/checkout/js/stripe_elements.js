27c2f1d026c74f4ecfdb8e9d0df3da2a47f10576
lbacon17 <louis.bacon17@icloud.com>
2021-02-13 18:14:28 +0000

    added stripe public and secret keys

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -21,3 +21,19 @@ var style = {
 var card = elements.create('card', {style, style});
 
 card.mount('#card-element');
+
+// Handle real time validation errors on the card element
+card.addEventListener('change', function(event) {
+    var errorDiv = document.getElementById('card-errors');
+    if (event.error) {
+        var html = `
+            <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+                <span>${event.error.message}</span>
+            </span>
+        `
+        $(errorDiv).html(html);
+    } else {
+        errorDiv.textContent = '';
+    };
+})
