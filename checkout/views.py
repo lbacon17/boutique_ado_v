e575248704e59d1ab72c550c18ba342464ea328e
lbacon17 <louis.bacon17@icloud.com>
2021-02-13 14:37:24 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout (request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51IKOUKFod4preCUvITDeh3wyeTZ1rYr7zYMXqO8v2DSTwMuySI2PCHdQr3IXdikVYarSUXhxGqI82EbQsWdzMbbR00SJtHasqE',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
